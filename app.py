62b78d3a5bdc6213af6d39f342e17ba9ccbbd7a9
goustg62@gmail.com <goustg62@gmail.com>
2024-05-25 11:38:14 +0300

    up css for index.html

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-    
+    return render_template('index.html', title="Pizza | Oderman")
+
+@app.route('/menu', methods=["POST", "GET"])
+def menu():
+    return "Hello!"
 
 app.run(host='0.0.0.0',port='80')
