8723a21a8fdf24eb7242b0edf8445cdd028536e2
goustg62@gmail.com <goustg62@gmail.com>
2024-06-08 11:18:59 +0300

    create base-jinja | nvbar menu

--- app.py
+++ app.py
@@ -2,16 +2,24 @@ from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
+navMenu = [
+    {"title": "Home",       "endpoint": "home"},
+    {"title": "Menu",       "endpoint": "menu"},
+
+]
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html', CSSpath="../static/css/404.css"), 404
 
 @app.route('/')
 def home():
-    return render_template('index.html', title="Pizza | Oderman")
+    return render_template('index.html', title="Pizza | Oderman", menu=navMenu, )
 
 @app.route('/menu', methods=["POST", "GET"])
 def menu():
     return "Hello!"
 
-app.run(host='0.0.0.0',port='80')
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
